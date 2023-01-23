5542f67b2ecd8c4b91c637f3921af86b5aa5a8d3
NogueiraLorena <118185045+NogueiraLorena@users.noreply.github.com>
2023-01-23 17:43:11 -0300

    Add files via upload

---
+++ 03_ExemploNew/src/controller/Cadastro.java
@@ -0,0 +1,24 @@
+package controller;
+import model.Usuario;
+public class Cadastro {
+
+	public static void main(String[] args) {
+		 Usuario usuario = new Usuario();
+		 
+		 usuario.nome = "Rafael";
+		 usuario.endereco = "Rua Truestreet, Nº 23";
+	
+	System.out.println("\n Usuário-> ");
+	System.out.println("Nome:"+usuario.nome);
+	System.out.println("Endereço: "+usuario.endereco);
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
